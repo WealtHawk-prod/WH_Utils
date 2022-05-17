fd932ce97ad1e7d1db6b4e5f07e7e2bc965d2acf
McClain <mcclain.thiel@gmail.com>
2022-05-16 21:00:45 -0700

    small change

--- WH_Utils/Analytics/Prospects/API.py
+++ WH_Utils/Analytics/Prospects/API.py
@@ -1,12 +1,32 @@
 from WH_Utils.Objects.Prospect import Prospect
+from WH_Utils.Analytics.Prospects.Nodes import get_company_data, classify_job_title
 
 from typing import List, Dict, Union, Optional
 
-def run_analytics(prospect: Prospect) ->  Dict[str, str]:
+
+def run_analytics(prospect: Prospect) -> Dict[str, str]:
     """
     Get all analytics for a prospect
 
     Currently supported analytics:
         - ect
     """
-    return
+
+    data = get_company_data(prospect)
+
+    try:
+        age = prospect.age
+
+    except:
+        age = None
+
+    data = {
+        "title": data['title'],
+        "employee_rank": classify_job_title([data['title']])[0],
+        "languages_spoken": prospect.languages,
+        "age": age,
+        "joined_pre_exit": data["joined_pre_exit?"],
+        "days_at_company_preexist": data['time_pre_exit']
+    }
+
+    return data
